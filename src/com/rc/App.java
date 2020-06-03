94984ffd26396b46896b6aeeb838841ae87846d8
Douglas Hirsh <douglas@codeup.com>
2020-06-03 10:14:12 -0500

    feat: Create initial login screen.

--- src/com/rc/App.java
+++ src/com/rc/App.java
@@ -1,11 +1,43 @@
 package com.rc;
 
+import com.rc.users.Guest;
 import com.rc.users.Registered;
 import com.rc.users.User;
+import com.rc.util.Input;
 
 public class App {
     public static void main(String[] args) {
         User fer = new Registered("Fernando", "Mendoza", "fer@mail.com", "fer", "pass");
 
+        Input input = new Input();
+
+        System.out.println("Welcome to Recipe Catcher");
+        //Register or Login or Proceed as guest
+        System.out.println("How do you want to proceed?\n" +
+                "1 - Register as a new user\n" +
+                "2 - Login\n" +
+                "3 - Proceed as Guest");
+        int userStatus = input.getInt(1, 3);
+        User currentUser = null;
+        switch (userStatus)  {
+            case 1:
+                System.out.println("Register as a new user.");
+                String firstName = input.getString("First Name: ");
+                String lastName = input.getString("Last Name: ");
+                String email = input.getString("Email: ");
+                String username = input.getString("Username: ");
+                String password = input.getString("Password: ");
+                currentUser = new Registered(firstName, lastName, email, username, password);
+                break;
+            case 2:
+                System.out.println("Login system is not implemented yet.");
+                break;
+            case 3:
+                System.out.println("Proceeding as guest.");
+                currentUser = new Guest(input.getString("First Name: "), input.getString("Last Name: "), input.getString("Email: "));
+                break;
+        }
+
+        System.out.println("currentUser = " + currentUser);
     }
 }
---
+++ src/com/rc/util/Input.java
@@ -0,0 +1,71 @@
+package com.rc.util;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner = new Scanner(System.in);
+
+//    public Input(){}
+
+    public String getString(){
+        return this.getString("Type something: ");
+    }
+
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo(){
+        System.out.println("Select y/n");
+        String answer = scanner.nextLine();
+        return answer.equals("y") ? true : false;
+    }
+
+    public int getInt(int min, int max){
+        int aNumber;
+        try {
+            aNumber = Integer.valueOf(this.getString("Give me a number between " + min + " and "  + max));
+        } catch (NumberFormatException e) {
+            return getInt(min, max);
+        }
+
+        if(aNumber < min || aNumber > max){
+            System.out.println("Outside of the range, try again");
+            return getInt(min, max);
+        }
+        return aNumber;
+    }
+
+    public int getInt(){
+        try {
+            return Integer.valueOf(this.getString("Give me an integer:"));
+        } catch (NumberFormatException e) {
+            return getInt();
+        }
+    }
+
+    public double getDouble(double min, double max){
+        double aDecimal;
+        try {
+            aDecimal = Double.valueOf(this.getString("Give me a decimal between " + min + " and "  + max));
+        } catch (NumberFormatException e) {
+            return getDouble(min, max);
+        }
+
+        if(aDecimal < min || aDecimal > max){
+            System.out.println("Outside of the range, try again");
+            return getDouble(min, max);
+        }
+        return aDecimal;
+    }
+
+    public double getDouble(){
+        try {
+            return Double.valueOf(this.getString("Give me a decimal:"));
+        } catch (NumberFormatException e) {
+            return getDouble();
+        }
+    }
+
+}
